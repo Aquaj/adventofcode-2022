21968d599e04e37640525c7ca6c215f86e85ab14
Jérémie <jeremie.bonal@gmail.com>
2022-12-02 06:25:29 +0100

    Pre-part 2 refacto

--- day-02.rb
+++ day-02.rb
@@ -3,19 +3,18 @@ require_relative 'common'
 class Day2 < AdventDay
   EXPECTED_RESULTS = { 1 => 15 }
 
-  OPPONENT_MOVES = {
-    'A' => :rock,
-    'B' => :paper,
-    'C' => :scissors,
-  }
-  MY_MOVES = {
-    'X' => :rock,
-    'Y' => :paper,
-    'Z' => :scissors,
-  }
+  OPPONENT_MOVES = { 'A' => :rock, 'B' => :paper, 'C' => :scissors }
+  MY_MOVES = { 'X' => :rock, 'Y' => :paper, 'Z' => :scissors }
+
+  WINNING_MOVES = [
+    [:paper, :rock],
+    [:scissors, :paper],
+    [:rock, :scissors],
+  ].freeze
+  LOSING_MOVES = WINNING_MOVES.map(&:reverse).freeze
 
   def first_part
-    rounds.sum do |(opponent_move, my_move)|
+    first_part_rounds.sum do |(opponent_move, my_move)|
       round_points = case winner(opponent_move, my_move)
       when 0  then 0 # lose
       when 1  then 6 # win
@@ -30,18 +29,17 @@ class Day2 < AdventDay
 
   private
 
+  def first_part_rounds
+    instructions.map do |(opponent_move, my_move)|
+      [OPPONENT_MOVES[opponent_move], MY_MOVES[my_move]]
+    end
+  end
+
   def winner(move_a, move_b)
     case
-    when move_a == :paper && move_b == :rock     then 0
-    when move_a == :scissors && move_b == :paper then 0
-    when move_a == :rock && move_b == :scissors  then 0
-
-    when move_a == :rock && move_b == :paper     then 1
-    when move_a == :paper && move_b == :scissors then 1
-    when move_a == :scissors && move_b == :rock  then 1
-
+    when WINNING_MOVES.include?([move_a, move_b]) then 0
+    when LOSING_MOVES.include?([move_a, move_b]) then 1
     else -1
-
     end
   end
 
@@ -54,12 +52,9 @@ class Day2 < AdventDay
   end
 
   def convert_data(data)
-    super.map do |rounds|
-      opponent_move, my_move = rounds.split
-      [OPPONENT_MOVES[opponent_move], MY_MOVES[my_move]]
-    end
+    super.map(&:split)
   end
-  alias_method :rounds, :input
+  alias_method :instructions, :input
 end
 
 Day2.solve
