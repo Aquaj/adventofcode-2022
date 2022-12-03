a8ae8a1687b5b67b8a89bd5c5d064179bcbd3fb7
Jérémie <jeremie.bonal@gmail.com>
2022-12-03 06:06:40 +0100

    Part one

--- day-03.rb
+++ day-03.rb
@@ -1,7 +1,13 @@
 require_relative 'common'
 
 class Day3 < AdventDay
+  EXPECTED_RESULTS = { 1 => 157, 2 => nil }
+
   def first_part
+    input.map { |ruck| [ruck[0...(ruck.length / 2)], ruck[(ruck.length / 2)..-1]] }.sum do |first_c, last_c|
+      common = (first_c.chars.uniq & last_c.chars.uniq)
+      [*('a'..'z'), *('A'..'Z')].index(common.unwrap) + 1
+    end
   end
 
   def second_part
