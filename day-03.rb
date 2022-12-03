9fef52c9a75a116782919f274954b540a130856d
Jérémie <jeremie.bonal@gmail.com>
2022-12-03 06:11:25 +0100

    Part 2

--- day-03.rb
+++ day-03.rb
@@ -1,7 +1,7 @@
 require_relative 'common'
 
 class Day3 < AdventDay
-  EXPECTED_RESULTS = { 1 => 157, 2 => nil }
+  EXPECTED_RESULTS = { 1 => 157, 2 => 70 }
 
   def first_part
     input.map { |ruck| [ruck[0...(ruck.length / 2)], ruck[(ruck.length / 2)..-1]] }.sum do |first_c, last_c|
@@ -11,6 +11,10 @@ class Day3 < AdventDay
   end
 
   def second_part
+    input.each_slice(3).sum do |group|
+      common = group.map(&:chars).reduce(:&).unwrap
+      [*('a'..'z'), *('A'..'Z')].index(common) + 1
+    end
   end
 
   private
