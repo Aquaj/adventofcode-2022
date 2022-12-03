50a20ed09e9f09fffd3a5674417ce2e440498acf
Jérémie <jeremie.bonal@gmail.com>
2022-12-03 06:21:00 +0100

    Post-exercise refacto

--- day-03.rb
+++ day-03.rb
@@ -3,25 +3,41 @@ require_relative 'common'
 class Day3 < AdventDay
   EXPECTED_RESULTS = { 1 => 157, 2 => 70 }
 
+  GROUP_SIZE = 3
+
   def first_part
-    input.map { |ruck| [ruck[0...(ruck.length / 2)], ruck[(ruck.length / 2)..-1]] }.sum do |first_c, last_c|
-      common = (first_c.chars.uniq & last_c.chars.uniq)
-      [*('a'..'z'), *('A'..'Z')].index(common.unwrap) + 1
+    rucksacks.sum do |rucksack|
+      priority_of(shared_item_in(compartments_of(rucksack)))
     end
   end
 
   def second_part
-    input.each_slice(3).sum do |group|
-      common = group.map(&:chars).reduce(:&).unwrap
-      [*('a'..'z'), *('A'..'Z')].index(common) + 1
+    rucksacks.each_slice(GROUP_SIZE).sum do |group|
+      priority_of(shared_item_in(group))
     end
   end
 
   private
 
+  # Lowercase item types a through z have priorities 1 through 26.
+  # Uppercase item types A through Z have priorities 27 through 52.
+  def priority_of(item)
+    [*('a'..'z'), *('A'..'Z')].index(item) + 1
+  end
+
+  def shared_item_in(collections)
+    collections.map(&:chars).reduce(&:&).unwrap
+  end
+
+  def compartments_of(rucksack)
+    midpoint = rucksack.length / 2
+    [rucksack[0...midpoint], rucksack[midpoint..-1]]
+  end
+
   def convert_data(data)
     super
   end
+  alias_method :rucksacks, :input
 end
 
 Day3.solve
