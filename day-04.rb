f30b14e33e6e770c4e47a80bf3f280032527b4f6
Jérémie <jeremie.bonal@gmail.com>
2022-12-04 06:16:49 +0100

    Post-exercise refacto

--- day-04.rb
+++ day-04.rb
@@ -4,20 +4,25 @@ class Day4 < AdventDay
   EXPECTED_RESULTS = { 1 => 2, 2 => 4 }
 
   def first_part
-    input.count { |s1,s2| s1.cover?(s2) || s2.cover?(s1) }
+    assignments.count { |first, second| first.cover?(second) || second.cover?(first) }
   end
 
   def second_part
-    input.count { |s1,s2| (s1.to_a & s2.to_a).any? }
+    assignments.count { |first, second| (first.to_a & second.to_a).any? }
   end
 
   private
 
   def convert_data(data)
-    super.map do |assign|
-      assign.split(',').map { |sec| sec.split('-').then { |(s,e)| (s.to_i..e.to_i) } }
+    super.map do |assignments|
+      first, second = assign.split(',')
+      [first, second].map do |assignment|
+        first_section, last_section = assignment.split('-')
+        (first_section..last_section)
+      end
     end
   end
+  alias_method :assignments, :input
 end
 
 Day4.solve
