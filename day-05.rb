84f0cf1d3b72be1afa7d4918d72f093338f26183
Jérémie <jeremie.bonal@gmail.com>
2022-12-05 06:18:37 +0100

    Part 1

--- day-05.rb
+++ day-05.rb
@@ -2,6 +2,10 @@ require_relative 'common'
 
 class Day5 < AdventDay
   def first_part
+    input[:instructions].each_with_object(input[:crates].dup) do |(how_many, from, to), crates|
+      to_move = how_many.times.map { crates[from - 1].shift }
+      to_move.each { |crate| crates[to - 1].unshift crate }
+    end.map(&:first).join
   end
 
   def second_part
@@ -10,7 +14,13 @@ class Day5 < AdventDay
   private
 
   def convert_data(data)
-    super
+    crates, instructions = data.split("\n\n")
+    *crates, _labels = crates.split("\n")
+    crates = crates.map do |row|
+      row.chars.each_slice(4).to_a.map { |crate| crate.join.strip[1] }
+    end.transpose.map(&:compact)
+    instructions = instructions.split("\n").map { |ins| ins.match(/move (\d+) from (\d+) to (\d+)/).captures.map(&:to_i) }
+    { crates: crates, instructions: instructions }
   end
 end
 
