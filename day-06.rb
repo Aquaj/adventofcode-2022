3f043061a3b5d2232f86cdbe17e029b3475c1c87
Jérémie <jeremie.bonal@gmail.com>
2022-12-06 06:10:09 +0100

    Part 1

--- day-06.rb
+++ day-06.rb
@@ -1,7 +1,17 @@
 require_relative 'common'
 
 class Day6 < AdventDay
+  EXPECTED_RESULTS = { 1 => 7, 2 => 19 }
+
   def first_part
+    stream = input.each_cons(4)
+    place = 4
+    chars = stream.next
+    until chars.uniq.size == chars.size
+      place += 1
+      chars = stream.next
+    end
+    place
   end
 
   def second_part
@@ -10,7 +20,7 @@ class Day6 < AdventDay
   private
 
   def convert_data(data)
-    super
+    super.unwrap.chars
   end
 end
 
