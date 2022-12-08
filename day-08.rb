62c4b4f3f003f5db8e16b6f7c14b137243b9ce31
Jérémie <jeremie.bonal@gmail.com>
2022-12-08 06:06:51 +0100

    Part 1

--- day-08.rb
+++ day-08.rb
@@ -1,7 +1,17 @@
 require_relative 'common'
 
 class Day8 < AdventDay
+  EXPECTED_RESULTS = { 1 => 21, 2 => nil }
+
   def first_part
+    tree_map.coords.count do |(x,y)|
+      current_tree = tree_map[x,y]
+
+      (0...x).all? { |n| tree_map[n,y] < current_tree } ||
+        (x+1...tree_map.width).all? { |n| tree_map[n,y] < current_tree } ||
+        (0...y).all? { |n| tree_map[x,n] < current_tree } ||
+        (y+1...tree_map.height).all? { |n| tree_map[x,n] < current_tree }
+    end
   end
 
   def second_part
@@ -10,8 +20,9 @@ class Day8 < AdventDay
   private
 
   def convert_data(data)
-    super
+    Grid.new(super.map(&:chars))
   end
+  alias_method :tree_map, :input
 end
 
 Day8.solve
