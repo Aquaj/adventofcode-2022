dc3bec8bd19dfba6b6b8e275dcaf53df196e6bde
Jérémie <jeremie.bonal@gmail.com>
2022-12-09 06:29:04 +0100

    Refacto pre-part 2

--- day-09.rb
+++ day-09.rb
@@ -5,7 +5,7 @@ class Day9 < AdventDay
 
   def first_part
     positions = { tail: [0,0], head: [0,0] }
-    tail_history = []
+    tail_history = Set.new.tap { |s| s << positions[:tail] }
     moves.each do |(direction, duration)|
       duration.times do
         xh,yh = *positions[:head]
@@ -23,35 +23,49 @@ class Day9 < AdventDay
           positions[:head] = [xh, yh-1]
         end
 
-        non_adjacent = [positions[:head], positions[:tail]].transpose.map { |pos| pos.reduce(&:-).abs }.any? { |d| d > 1 }
-        if non_adjacent
-          # Move tail
-          if xh > xt
-            new_xt = xt + 1
-          elsif xh < xt
-            new_xt = xt - 1
-          else
-            new_xt = xt
-          end
-          if yh > yt
-            new_yt = yt + 1
-          elsif yh < yt
-            new_yt = yt - 1
-          else
-            new_yt = yt
-          end
-          positions[:tail] = [new_xt, new_yt]
+        if !adjacent?(positions[:head], positions[:tail])
+          new_coords = move_towards(positions[:tail], positions[:head])
+          positions[:tail] = new_coords
           tail_history << positions[:tail].dup
         end
       end
     end
-    tail_history.uniq.count + 1
+    tail_history.size
   end
 
   def second_part
   end
 
   private
+
+  def adjacent?(pos1, pos2)
+    x1,y1 = *pos1
+    x2,y2 = *pos2
+    (x1 - x2).abs <= 1 && (y1 - y2).abs <= 1
+  end
+
+  def move_towards(curr, dest)
+    x1,y1 = curr
+    x2,y2 = dest
+
+    if x2 > x1
+      new_x1 = x1 + 1
+    elsif x2 < x1
+      new_x1 = x1 - 1
+    else
+      new_x1 = x1
+    end
+
+    if y2 > y1
+      new_y1 = y1 + 1
+    elsif y2 < y1
+      new_y1 = y1 - 1
+    else
+      new_y1 = y1
+    end
+
+    [new_x1, new_y1]
+  end
 
   def convert_data(data)
     super.map do |move|
