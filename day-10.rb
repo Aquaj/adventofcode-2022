597f0d75b6720b4d87dc42fcf05db8f729a8f40e
Jérémie <jeremie.bonal@gmail.com>
2022-12-10 09:46:15 +0100

    Part 1

--- day-10.rb
+++ day-10.rb
@@ -1,7 +1,43 @@
 require_relative 'common'
 
 class Day10 < AdventDay
+  EXPECTED_RESULTS = { 1 => 13140, 2 => nil }
+
+
+  class CPU
+    SIGNAL_TICKS = (0...6).map { |n| 20 + 40 * n }
+
+    attr_reader :signals
+
+    def initialize
+      @registers = { X: 1 }
+      @clock = 1
+      @signals = SIGNAL_TICKS.map { |tick| [tick, nil] }.to_h
+    end
+
+    def run(instructions)
+      instructions.each do |instruction, arg|
+        case instruction
+        when 'addx'
+          tick!
+          tick! { @registers[:X] += arg }
+        when 'noop'
+          tick!
+        end
+      end
+    end
+
+    def tick!
+      @clock += 1
+      yield if block_given?
+      @signals[@clock] = @clock * @registers[:X] if @signals.key?(@clock)
+    end
+  end
+
   def first_part
+    cpu = CPU.new
+    cpu.run(instructions)
+    cpu.signals.values.sum
   end
 
   def second_part
@@ -10,8 +46,12 @@ class Day10 < AdventDay
   private
 
   def convert_data(data)
-    super
+    super.map do |cmd|
+      instr, arg = cmd.split
+      [instr, arg&.to_i]
+    end
   end
+  alias_method :instructions, :input
 end
 
 Day10.solve
