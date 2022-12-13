df2221d720e7a241501b0ab1cf1a5ceee5ac787b
Jérémie <jeremie.bonal@gmail.com>
2022-12-13 07:04:32 +0100

    Refactored mutability out

--- day-13.rb
+++ day-13.rb
@@ -5,54 +5,63 @@ class Day13 < AdventDay
 
   def first_part
     packet_pairs.filter_map.with_index do |pair, index|
-      index + 1 if compare(*pair.deep_copy)
+      index + 1 if compare(*pair)
     end.sum
   end
 
   DIVIDER_PACKETS = [ [[2]], [[6]] ]
   def second_part
-    sorted = (packets + DIVIDER_PACKETS).sort { |*pair| compare(*pair.deep_copy) ? -1 : 1 }
+    sorted = (packets + DIVIDER_PACKETS).sort { |*pair| compare(*pair) ? -1 : 1 }
     DIVIDER_PACKETS.map { |packet| sorted.index(packet) + 1 }.reduce(&:*)
   end
 
   private
 
   def compare(packet_1, packet_2)
-    value_1 = packet_1.shift
-    value_2 = packet_2.shift
+    value_1, *remainder_1 = packet_1
+    value_2, *remainder_2 = packet_2
+
+    pair = [value_1, value_2]
     case
-    when [value_1, value_2].all? { |val| val.is_a? Numeric }
+    # both nums
+    when match(pair, [Numeric, Numeric])
       return true if value_1 < value_2
       return false if value_1 > value_2
-      return compare(packet_1, packet_2)
-    when [value_1, value_2].all? { |val| val.is_a? Array }
+      return compare(remainder_1, remainder_2)
+
+    # both lists
+    when match(pair, [Array, Array])
       is_valid = compare(value_1, value_2)
-      return compare(packet_1, packet_2) if is_valid.nil?
+      return compare(remainder_1, remainder_2) if is_valid.nil?
       return is_valid
-    when value_1.is_a?(Numeric) && value_2.is_a?(Array)
-      packet_1.unshift([value_1])
-      packet_2.unshift(value_2)
-      compare(packet_1, packet_2)
-    when value_1.is_a?(Array) && value_2.is_a?(Numeric)
-      packet_1.unshift(value_1)
-      packet_2.unshift([value_2])
-      compare(packet_1, packet_2)
+
+    # 1 is num, 1 is list
+    when match(pair, [Numeric, Array])
+      compare([Array(value_1), *remainder_1], packet_2)
+    when match(pair, [Array, Numeric])
+      compare(packet_1, [Array(value_2), *remainder_2])
+
     # End of packet checks
-    when [value_1, value_2] == [nil, nil]
+    when pair.all?(&:nil?)
       return nil
     when value_1.nil?
       return true
     when value_2.nil?
       return false
+
     else
       raise "Strange packet state uncovered"
     end
   end
 
+  def match(pair, classes)
+    pair.zip(classes).all? { |obj, klass| obj.is_a? klass }
+  end
+
   def convert_data(data)
     data.split("\n\n").map do |packet_pair|
       packet_pair.split("\n").map do |packet|
-        raise "Dangerous packet" unless packet.match? /^[\d,\[\]]+$/
+        raise "Dangerous packet" unless packet.match? /^[\d,\[\] ]+$/
         eval(packet)
       end
     end
