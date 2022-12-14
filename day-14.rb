60f9f8f218be76609c930bfe81fd5d4720d1164d
Jérémie <jeremie.bonal@gmail.com>
2022-12-14 08:09:39 +0100

    Part 2

--- day-14.rb
+++ day-14.rb
@@ -16,6 +16,13 @@ class Day14 < AdventDay
   end
 
   def second_part
+    occupied_spaces = rock_coords.deep_copy
+
+    (1..).find do |sand_unit|
+      resting_place = fall_from(SAND_SOURCE, obstacles: occupied_spaces)
+      occupied_spaces << resting_place
+      resting_place == SAND_SOURCE
+    end
   end
 
   private
@@ -36,6 +43,12 @@ class Day14 < AdventDay
   class Obstacles
     def initialize
       @blocks = {}
+      @floor_y = nil
+    end
+
+    def finalize!
+      lowest_block = @blocks.values.reduce(&:|).max
+      @floor_y = lowest_block + 2
     end
 
     def <<(coords)
@@ -47,12 +60,14 @@ class Day14 < AdventDay
     def include?(coords)
       x,y = *coords
       return true if @blocks[x]&.include? y
+      return true if @floor_y && (y >= @floor_y)
     end
 
     def under(coords)
       source_x, source_y = coords
       y_under = @blocks[source_x]&.select { |y| y > source_y }&.min
-      [source_x, y_under] if y_under
+      first_under = [source_x, y_under] if y_under
+      first_under || ([coords[0], @floor_y] if @floor_y)
     end
   end
 
@@ -64,7 +79,7 @@ class Day14 < AdventDay
         x1.towards(x2).each { |x| spaces << [x,y1] } # y1 == y2
         y1.towards(y2).each { |y| spaces << [x1,y] } # x1 == x2
       end
-    end
+    end.tap { |o| o.finalize! if @part == 2 }
   end
 
   def convert_data(data)
