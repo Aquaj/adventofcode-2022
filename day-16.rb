8bf23e7fe7211b26dd7bd442160603c2b4a818b4
Jérémie <jeremie.bonal@gmail.com>
2022-12-17 17:58:13 +0100

    As good as P2 is going to get

--- day-16.rb
+++ day-16.rb
@@ -1,55 +1,82 @@
 require_relative 'common'
-require 'algorithms'
+
+require_relative 'day-16/models'
+require_relative 'day-16/algorithms'
 
 class Day16 < AdventDay
-  EXPECTED_RESULTS = { 1 => 1651, 2 => nil }
+  EXPECTED_RESULTS = { 1 => 1651, 2 => 1707 }
 
+  ELEPHANT_TEACHING_DURATION = 4
   MINUTES_BEFORE_ERUPTION = 30
   START_VALVE_NAME = "AA"
 
-  def first_part
-    @count = @cache = 0
-    optimized_valveset = optimize(valves)
-    path = optimal_moves(optimized_valveset.find { |v| v.name == START_VALVE_NAME })
-    puts path.map(&:inspect)
+  def first_part(optimize: true)
+    search = valves
+    search = optimize(search) if optimize
+
+    start = search.find { |v| v.name == START_VALVE_NAME }
+
+    path = optimal_moves(start)
     total_pressure(path)[:pressure]
+
+    # args = State.new({
+    #   actors: Set.new([
+    #     Actor.new(name: 'Human', position: start, path: [], time: MINUTES_BEFORE_ERUPTION),
+    #   ]),
+    #   remaining_valves: Set.new(valves),
+    #   pressure: 0,
+    # })
+    # bfs_elephant_moves(args)
   end
 
-  Graph = Struct.new(:nodes, :edges) do
-    def edge_cost(v,n)
-      v.neighbors[n]
-    end
-  end
-
-  def optimize(start)
-    edges = valves.flat_map { |v| v.neighbors.map { |n,d| [v, n] } }
-    graph = Graph.new(valves, edges)
-    new_valves = valves.map(&:dup)
-    new_valves.each do |valve|
-      old = valves.find { |v| v.name == valve.name }
-      new_distances = Algorithms.bellman_ford(old, graph)[:distances]
-      valve.neighbors = new_distances.without(valve).map do |old, distance|
-        new = new_valves.find { |v| v.name == old.name }
-        [new, distance]
-      end.reject { |v,_| v.jammed? }.sort_by { |v,_| v.flow_rate }.to_h
-    end
-    new_valves.to_set
+  def display_path(path)
+    puts path.map(&:inspect)
   end
 
   def second_part
+    # bfs_second # Works but not performant enough
+    dfs_second
   end
 
   private
 
+  # Fully modelized
+  def bfs_second
+    start = valves.find { |v| v.name == START_VALVE_NAME }
+    remaining_time = MINUTES_BEFORE_ERUPTION - ELEPHANT_TEACHING_DURATION
+
+    args = State.new({
+      actors: Set.new([
+        Actor.new(name: 'Human', position: start, path: [], time: remaining_time),
+        Actor.new(name: 'Elephant', position: start, path: [], time: remaining_time),
+      ]),
+      remaining_valves: Set.new(valves),
+      pressure: 0,
+    })
+
+    bfs_elephant_moves(args).pressure
+  end
+
+  # Not fully moelized for performance
+  def dfs_second(low_object: false)
+    start = valves.find { |v| v.name == START_VALVE_NAME }
+    remaining_time = MINUTES_BEFORE_ERUPTION - ELEPHANT_TEACHING_DURATION
+
+    if low_object
+      res = low_objects_dfs(start, start, time: remaining_time) # Even fewer objects to go even faster
+      res.flatten.max
+    else
+      res = dfs_elephant_moves(start, start, time: remaining_time)
+      res.values.max
+    end
+  end
+
   def optimal_moves(current_valve, time: MINUTES_BEFORE_ERUPTION, open_valves: Set.new, cache: {})
-    return [] if time == 0
-    return ([WAIT]*time) if open_valves.count == valves.reject { |v| v.jammed? }.count
+    return [] if time <= 0
 
     cache_key = [current_valve.hash, time, open_valves.map(&:hash).sort]
 
-    p @count if (@count += 1) % 1000 == 0
     cache[cache_key] ||= begin
-      @cache += 1
       paths = current_valve.actions.filter_map do |action|
         next if open_valves.include? action.opens_valve
         next if time - action.time_cost < 0
@@ -62,7 +89,7 @@ class Day16 < AdventDay
         )
       end
       max = paths.max_by {|e| total_pressure(e)[:pressure] }
-      max || ([WAIT]*time) # If we can't do anything...
+      max
     end
   end
 
@@ -73,32 +100,12 @@ class Day16 < AdventDay
     end
   end
 
-  Action = Struct.new(:time_cost, :move_to, :opens_valve, keyword_init: true)
-  class Travel < Action; def inspect = "Travel<#{move_to.name} : #{time_cost}mn>"; end
-  class ValveOpening < Action; def inspect = "Open<#{opens_valve.name} +#{opens_valve.flow_rate}>"; end
-  WAIT = Action.new(time_cost: 1, move_to: self, opens_valve: nil).tap { |w| def w.inspect = 'WAIT' }
-
-  Valve = Struct.new(:name, :flow_rate, :neighbors, keyword_init: true) do
-    def inspect = "#{self.class.name}<#{name}: #{flow_rate} — #{neighbors.transform_keys(&:name).inspect}>"
-    def to_s = inspect
-    def hash = object_id
-    def jammed? = flow_rate == 0
-    def actions
-      [
-        (ValveOpening.new(time_cost: 1, move_to: self, opens_valve: self) unless jammed?),
-        *neighbors.map do |neighbor, distance|
-          Travel.new(time_cost: distance, move_to: neighbor, opens_valve: nil)
-        end.sort_by(&:time_cost)
-      ].compact
-    end
-  end
-
   FORMAT = /Valve (\w+) has flow rate=(\d+); tunnels? leads? to valves? (.*)/
   def convert_data(data)
-    super.map do |valve|
+    super.map.with_index do |valve, index|
       info = valve.match(FORMAT)&.captures
       raise "Bad valve format" unless info
-      Valve.new(name: info[0], flow_rate: info[1].to_i, neighbors: info[2].split(', '))
+      Valve.new(name: info[0], index: index, flow_rate: info[1].to_i, neighbors: info[2].split(', '))
     end.then do |valves|
       # Convert neighbors from names to the actual Valve objects
       valves.each do |valve|
