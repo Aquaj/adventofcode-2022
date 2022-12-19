5d04a32a4eb164b8ce279953d0e9c40bd12fc449
Jérémie <jeremie.bonal@gmail.com>
2022-12-19 18:51:59 +0100

    Part 1

--- day-19.rb
+++ day-19.rb
@@ -1,7 +1,15 @@
 require_relative 'common'
+require 'z3'
 
 class Day19 < AdventDay
+  EXPECTED_RESULTS = { 1 => 33, 2 => nil }
+
+  MINUTES_TO_EXTRACT = 24
+
+  RESOURCES = %i[ore clay obsidian geode]
+
   def first_part
+    blueprints.map { |index, blueprint| index * max_geodes_with(blueprint) }.sum
   end
 
   def second_part
@@ -9,9 +17,69 @@ class Day19 < AdventDay
 
   private
 
-  def convert_data(data)
-    super
+  def max_geodes_with(blueprint)
+    solver = Z3::Optimize.new
+
+    problem = Array.new(MINUTES_TO_EXTRACT + 1) do |time|
+      {
+        spent: RESOURCES.map { |r| [r, Z3::Int("spent[#{time}][#{r}]")] }.to_h,
+        built: RESOURCES.map { |r| [r, Z3::Int("built[#{time}][#{r}]")] }.to_h,
+        robots: RESOURCES.map { |r| [r, Z3::Int("robots[#{time}][#{r}]")] }.to_h,
+        resources: RESOURCES.map { |r| [r, Z3::Int("resources[#{time}][#{r}]")] }.to_h,
+      }
+    end
+
+    (1..MINUTES_TO_EXTRACT).each do |time|
+      resource_evolution = RESOURCES.map do |resource|
+        existing_resource = problem[time - 1][:resources][resource]
+        new_resource = problem[time - 1][:robots][resource]
+
+        robot_costs = blueprint.map do |type, costs|
+          problem[time][:built][type] * costs.fetch(resource, 0)
+        end.reduce(&:+)
+
+        (problem[time][:spent][resource] == robot_costs) &
+          (existing_resource - problem[time][:spent][resource] >= 0) &
+          (problem[time][:resources][resource] == existing_resource + new_resource - problem[time][:spent][resource])
+      end
+
+      robot_evolution = RESOURCES.map do |resource|
+        existing_robots = problem[time - 1][:robots][resource]
+        problem[time][:robots][resource] == existing_robots + problem[time][:built][resource]
+      end
+
+      solver.assert Z3::And(
+        *problem[time][:built].map { |_,c| c >= 0 },
+        *problem[time][:built].map { |_,c| c <= 1 },
+        problem[time][:built].values.sum <= 1,
+        *problem[time][:spent].map { |_,c| c >= 0 },
+        *resource_evolution,
+        *robot_evolution
+      )
+    end
+
+    solver.assert Z3::And(
+      *RESOURCES.map { |resource| problem[0][:resources][resource] == 0 },
+      *(RESOURCES - [:ore]).map { |resource| problem[0][:robots][resource] == 0 },
+      problem[0][:robots][:ore] == 1,
+    )
+
+    solver.maximize problem[24][:resources][:geode]
+
+    solver.model[problem[24][:resources][:geode]].to_i if solver.satisfiable?
   end
+
+  def convert_data(data)
+    super.map do |blueprint|
+      id, definitions = blueprint.match(/Blueprint (\d+): (.*)/).captures
+      robots = definitions.scan /Each (?<type>\w+) robot costs (?<costs>.*?)\./
+      robots = robots.map do |type, costs|
+        [type.to_sym, costs.scan(/(\d+) (\w+)/).to_h.reverse.transform_keys(&:to_sym).transform_values(&:to_i)]
+      end.to_h
+      [id.to_i, robots]
+    end.to_h
+  end
+  alias_method :blueprints, :input
 end
 
 Day19.solve
