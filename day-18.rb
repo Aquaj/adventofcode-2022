0b1bb5c8e1d4d974074c6b0f9b3b5c3bd7136c6b
Jérémie <jeremie.bonal@gmail.com>
2022-12-19 01:34:39 +0100

    Part 2 refacto

--- day-18.rb
+++ day-18.rb
@@ -6,31 +6,32 @@ class Day18 < AdventDay
   def first_part
     cubes = compute_neighbors(lava_coords)
 
-    cubes.sum { |cube, neighbors| 6 - neighbors.count }
+    surface_area(cubes)
   end
 
   LavaCube = Struct.new(:coords, :neighbors)
   def second_part
-    air = Set.new
-    cubes.each do |coords, cube|
-      lava, air_cubes = neighbors_3d(coords).partition { |pos| cubes[pos] }
-      cube.neighbors = lava.map { |c| cubes[c] }
-      air += air_cubes
-    end
-    pockets = air.reduce(components) do |bubble|
-      pocket = full_pocket bubble, cubes.values
-    end
-    air = pockets.reduce(&:+)
-    air_neighbors = {}
-    air.each do |coords|
-      neighbors = neighbors_3d(coords)
-      air_neighbors[coords] = neighbors.select { |n| air.include? n }
+
+    lava, air = compute_neighbors(lava_coords, track_adjacent: true)
+
+    # compute pockets
+    trapped_air = Set.new
+    pockets = air.filter_map do |bubble|
+      next if trapped_air.include? bubble
+      pocket = full_pocket bubble, lava.keys.to_set
+      next unless pocket
+      trapped_air += pocket
+      compute_neighbors(pocket)
     end
 
-    cubes.count * 6 - cubes.values.map(&:neighbors).map(&:count).sum - pockets.uniq.map { |p| p.count * 6 - p.sum { |c| air_neighbors[c].count } }.sum
+    surface_area(lava) - pockets.sum { |pocket| surface_area(pocket) }
   end
 
   private
+
+  def surface_area(glob)
+    glob.sum { |(cube, neighbors)| 6 - neighbors.count }
+  end
 
   def neighbors_3d(coords)
     x,y,z = *coords
@@ -41,30 +42,36 @@ class Day18 < AdventDay
     ]
   end
 
-  def compute_neighbors(cubes)
-    cubes.each_with_object({}) do |coords, neighbors|
+  def compute_neighbors(cubes, track_adjacent: false)
+    cubes.each_with_object([{}.with_default(Set.new), Set.new]) do |coords, (neighbors_of, adjacent)|
       neighbors_pos = neighbors_3d(coords)
-      neighbors[coords] = neighbors_pos.select { |n| cubes.include? n }
-    end
+      neighbors, not_neighbors = neighbors_pos.partition { |n| cubes.include? n }
+
+      neighbors_of[coords] = neighbors.to_set
+      not_neighbors.each { |nn| adjacent << nn } if track_adjacent
+    end.then { |(n,a)| track_adjacent ? [n,a] : n }
   end
 
   # BFS-ing our way into either the wall (pocket)
   # or the outside world (not pocket)
-  def full_pockets(coord, glob)
-    min_x, max_x = glob.map { |c| c[0] }.minmax
-    min_y, max_y = glob.map { |c| c[1] }.minmax
-    min_z, max_z = glob.map { |c| c[2] }.minmax
+  def full_pocket(coord, lava)
+    # Compute total bounds
+    min_x, max_x = lava.map { |c| c[0] }.minmax
+    min_y, max_y = lava.map { |c| c[1] }.minmax
+    min_z, max_z = lava.map { |c| c[2] }.minmax
 
     current = coord
     queue = []
     discovered = Set.new([current])
     until current.nil? do
       neighbors_3d(current).each do |node|
-        next if discovered.include?(node) || glob.include?(node)
+        next if discovered.include?(node) || lava.include?(node)
 
+        # Our bounds would be the lava if we were completely contained
         return false if node[0] < min_x || node[0] > max_x
         return false if node[1] < min_y || node[1] > max_y
         return false if node[2] < min_z || node[2] > max_z
+
         discovered << node
         queue << node
       end
@@ -75,7 +82,7 @@ class Day18 < AdventDay
   end
 
   def convert_data(data)
-    super.map { |cube| cube.split(',').map(&:to_i) }
+    super.map { |cube| cube.split(',').map(&:to_i) }.to_set
   end
   alias_method :lava_coords, :input
 end
