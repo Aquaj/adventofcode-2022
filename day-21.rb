57e31d2aa37d45e03ac3b4da8c8a3155919f4023
Jérémie <jeremie.bonal@gmail.com>
2022-12-21 12:49:27 +0100

    Part 2

--- day-21.rb
+++ day-21.rb
@@ -1,14 +1,16 @@
 require_relative 'common'
 require 'rltk'
+require 'z3'
 
 class Day21 < AdventDay
-  EXPECTED_RESULTS = { 1 => 152, 2 => nil }
+  EXPECTED_RESULTS = { 1 => 152, 2 => 301 }
 
   class MonkeyLexer < RLTK::Lexer
-    rule(/\+/) { [:PLS, -> (a,b) { a + b }] }
-    rule(/-/)  { [:SUB, -> (a,b) { a - b }] }
-    rule(/\*/) { [:MUL, -> (a,b) { a * b }] }
-    rule(/\//) { [:DIV, -> (a,b) { a / b }] }
+    rule(/\+/) { [:PLS, -> (a,b) { a + b  }] }
+    rule(/-/)  { [:SUB, -> (a,b) { a - b  }] }
+    rule(/\*/) { [:MUL, -> (a,b) { a * b  }] }
+    rule(/\//) { [:DIV, -> (a,b) { a / b  }] }
+    rule(/=/)  { [:EQL, -> (a,b) { a == b }] }
 
     rule(/[0-9]+/) { |t| [:NUM, t.to_i] }
 
@@ -79,6 +81,7 @@ class Day21 < AdventDay
       clause('expression SUB expression') { |e0, op, e1| Operation.new(op, e0, e1) }
       clause('expression MUL expression') { |e0, op, e1| Operation.new(op, e0, e1) }
       clause('expression DIV expression') { |e0, op, e1| Operation.new(op, e0, e1) }
+      clause('expression EQL expression') { |e0, op, e1| Operation.new(op, e0, e1) }
     end
 
     production(:variable) do
@@ -103,6 +106,24 @@ class Day21 < AdventDay
   end
 
   def second_part
+    new_instructions = instructions.gsub(/root: (\w*) . (\w*)/, 'root: \1 = \2')
+
+    tokens = MonkeyLexer.lex(new_instructions)
+    ast = MonkeyParser.parse(tokens)
+    runtime = ast.each_with_object({}) do |instruction, context|
+      instruction.resolve(context)
+    end
+
+    solver = Z3::Solver.new
+    answer = Z3::Int('?')
+
+    # Modifying runtime before we run `root` to 'record'
+    # all that will need to happen to make the numbers match
+    runtime['humn'] = -> { answer }
+    equality = runtime['root'].call
+
+    solver.assert equality
+    solver.model[answer].to_i if solver.satisfiable?
   end
 
   private
